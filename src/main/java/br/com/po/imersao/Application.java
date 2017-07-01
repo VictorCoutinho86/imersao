18346a10bd11bdd53aee4fbbb9efa8926b982b30
Victor Coutinho <victor.melo@aedu.com>
2017-07-01 16:12:39 -0300

    Corrigindo o application

---
+++ src/main/java/br/com/po/imersao/Application.java
@@ -0,0 +1,14 @@
+package br.com.po.imersao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	//Inicializa a aplicação 
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
