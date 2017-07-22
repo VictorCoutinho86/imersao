c8f2edf237d4e15e230a74c401bb23eff4259fcd
bcnogueira <bcnogueira@id.uff.br>
2017-07-21 23:02:04 -0300

    Breno Nogueira: Adicionando anotacao para poder rodar no projeto angular.

--- src/main/java/br/com/po/imersao/controller/UsuarioController.java
+++ src/main/java/br/com/po/imersao/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import br.com.po.imersao.model.Usuario;
 import br.com.po.imersao.repository.UsuarioRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @Controller
 @RequestMapping(path="/user")
 public class UsuarioController {
